1979a42aed960c54bd6ab1e686c1c010f65770d1
Nicole Slater <nicoleslater@pursuit.org>
2023-10-08 16:43:14 -0400

    controllers file config

--- controllers/locationsController.js
+++ controllers/locationsController.js
@@ -1,3 +1,14 @@
 const locations = express.Router();
 
 const locationData = require("../models/location");
+
+
+app.get("/locations", (req, res) => {
+    const indexOfArray = parseInt(req.params.indexOfArray);
+
+    if (isNaN(indexOfArray) || indexOfArray < 0 || indexOfArray >= locationData.length){
+        res.status(404).send(`Sorry, that location cannot be found at /location/${indexOfArray}`)
+    } else {
+        const location = locationData
+    }
+})
