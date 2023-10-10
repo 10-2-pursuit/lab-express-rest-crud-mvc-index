2aad8d18f4d0782bd2576eaa1893001f1c2802ba
Shanice <shanicegriffin@pursuit.org>
2023-10-10 17:19:56 -0400

    all tests passed

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const locations = require("./controllers/locations.controller.js");
+const persons = require("./controllers/persons.controller.js");
+const machines = require("./controllers/machines.controller.js");
+const plans = require("./controllers/plans.controller.js");
+const specialEvents = require("./controllers/special-events.controller.js");
+
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hello, world!")
 })
+
+app.use("/locations", locations);
+
+app.use("/persons", persons);
+
+app.use("/machines", machines);
+
+app.use("/plans", plans);
+
+app.use("/special-events", specialEvents)
 
 module.exports = app;
