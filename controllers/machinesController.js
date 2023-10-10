36abbb1caa3ac5706269e62734447c926f647f99
Nicole Slater <nicoleslater@pursuit.org>
2023-10-10 19:55:07 -0400

    Updating machine controller

---
+++ controllers/machinesController.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const machines = express.Router();
+
+const machinesData = require("../models/machine");
+
+machines.get("/", (req, res) => {
+    console.log("Send all machine data")
+    res.json(machinesData);
+})
+
+
+module.exports = machines;
