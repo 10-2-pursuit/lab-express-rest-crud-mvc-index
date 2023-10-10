4bd6c049efec434bcf65a10a43fa516759d572d1
arim83 <jorammercado@pursuit.org>
2023-10-10 13:10:33 -0400

    bonus complete

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -1,9 +1,24 @@
 const express = require("express")
 const locations = express.Router()
 const locationsArray = require("../models/locations.model.js")
+const peopleArray = require("../models/persons.model.js")
 
 locations.get("/", (req,res) => {
     res.json(locationsArray)
+})
+
+locations.get("/people", (req,res) => {
+    const response = [...locationsArray]
+    for(const location of response){
+        if(location.people===null||response.people===undefined)
+            location.people = []
+        for(const person of peopleArray){
+            if(location.zip===person.mainLocation){
+                location.people.push(person)
+            }
+        }
+    }
+    res.json(response)
 })
 
 module.exports = locations
